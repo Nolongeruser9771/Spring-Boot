d1c091745106d0dc523cef24b267f89e9d126c59
nguyenntt <106979668+Nolongeruser9771@users.noreply.github.com>
2023-05-23 09:51:29 +0700

    Huong's wedding

---
+++ wedding-of-huong-san/src/main/java/com/example/weddingofhuongsan/WeddingOfHuongSanApplication.java
@@ -0,0 +1,13 @@
+package com.example.weddingofhuongsan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingOfHuongSanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeddingOfHuongSanApplication.class, args);
+    }
+
+}
---
+++ wedding-of-huong-san/src/test/java/com/example/weddingofhuongsan/WeddingOfHuongSanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weddingofhuongsan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingOfHuongSanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
