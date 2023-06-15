6f09f5c8f26eb5d0870d29f51d98aed3c8b1a765
nguyenntt <106979668+Nolongeruser9771@users.noreply.github.com>
2023-06-15 17:08:42 +0700

    delete unused file

--- user-app-thymeleaf/src/main/java/com/example/userapp/controller/FileServerController.java
+++ user-app-thymeleaf/src/main/java/com/example/userapp/controller/FileServerController.java
@@ -5,17 +5,36 @@ import com.example.userapp.service.FileServerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-
-@Controller
+@RestController
 @RequestMapping("")
 public class FileServerController {
     @Autowired
     private FileServerService fileServerService;
 
+    //1. *** Upload file ***
+    @PostMapping("api/v1/users/{userId}/files")
+    public FileServer uploadFile(@ModelAttribute("fileUpload") MultipartFile fileUpload,
+                                 @PathVariable Integer userId) {
+        FileServer fileServer = fileServerService.uploadFile(userId, fileUpload);
+        return fileServer;
+    }
 
+    //2. Đọc file
+    @GetMapping("api/v1/files/{id}")
+    public ResponseEntity<?> readFile(@PathVariable Integer id) {
+        FileServer fileServer = fileServerService.getFileById(id);
+        return ResponseEntity.ok() //thông báo cho trình duyệt về kiểu dữ liệu trả về
+                .contentType(MediaType.parseMediaType(fileServer.getType()))
+                .body(fileServer.getData());
+    }
+
+    //3. Xóa file
+    @DeleteMapping("api/v1/files/{id}")
+    public ResponseEntity<?> deleteFile(@PathVariable Integer id){
+        fileServerService.deleteFile(id);
+        return ResponseEntity.noContent().build();
+    }
 }
